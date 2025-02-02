db8cb71c2347e87dc240112083b1e3b5332d1db0
MohammedKaif037 <kaifmohammed037@gmail.com>
2025-02-02 19:30:12 +0530

    Initial Commit XYD

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,12 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
+import App from './App';
 import './index.css';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) throw new Error('Root element not found');
 
-const root = createRoot(rootElement);
-root.render(
+createRoot(rootElement).render(
   <StrictMode>
     <App />
   </StrictMode>
